7e98626d8d6c53300e5208e035a55f7cc75e57f9
Andrés Bustamante <afbustag@gmail.com>
2026-07-18 09:15:37 +0200

    Separate configuration for caching and task execution in asynchronous mode

--- mystore-app/src/main/java/net/andresbustamante/mystore/MyStoreWebApplication.java
+++ mystore-app/src/main/java/net/andresbustamante/mystore/MyStoreWebApplication.java
@@ -3,15 +3,9 @@ package net.andresbustamante.mystore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan(basePackages = "net.andresbustamante.mystore.jpa.entities")
-@EnableScheduling
-@EnableAsync
-@EnableCaching
 public class MyStoreWebApplication {
 
     public static void main(String[] args) {
---
+++ mystore-core/src/main/java/net/andresbustamante/mystore/core/config/CachingConfig.java
@@ -0,0 +1,9 @@
+package net.andresbustamante.mystore.core.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+}
